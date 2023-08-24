2b4125d909dba1dff48f67a7061f48f866b2103e
Isaih7779 <crjordan436@gmail.com>
2023-08-24 15:30:13 +0530

    Operations in python

---
+++ BasicOperations.py
@@ -0,0 +1,94 @@
+# Arithmetic Operations
+a = 10
+b = 5
+
+# Addition
+addition_result = a + b
+print("Addition:", addition_result)
+
+# Subtraction
+subtraction_result = a - b
+print("Subtraction:", subtraction_result)
+
+# Multiplication
+multiplication_result = a * b
+print("Multiplication:", multiplication_result)
+
+# Division
+division_result = a / b
+print("Division:", division_result)
+
+# Modulus (Remainder)
+modulus_result = a % b
+print("Modulus:", modulus_result)
+
+# Exponentiation
+exponentiation_result = a ** b
+print("Exponentiation:", exponentiation_result)
+
+# Floor Division (Integer Division)
+floor_division_result = a // b
+print("Floor Division:", floor_division_result)
+
+# Comparison Operators
+x = 10
+y = 20
+
+# Equal to
+print("Is x equal to y?", x == y)
+
+# Not equal to
+print("Is x not equal to y?", x != y)
+
+# Greater than
+print("Is x greater than y?", x > y)
+
+# Less than
+print("Is x less than y?", x < y)
+
+# Greater than or equal to
+print("Is x greater than or equal to y?", x >= y)
+
+# Less than or equal to
+print("Is x less than or equal to y?", x <= y)
+
+# Logical Operators
+p = True
+q = False
+
+# AND
+print("p AND q:", p and q)
+
+# OR
+print("p OR q:", p or q)
+
+# NOT
+print("NOT p:", not p)
+
+# Bitwise Operators
+m = 5
+n = 3
+
+# Bitwise AND
+bitwise_and_result = m & n
+print("Bitwise AND:", bitwise_and_result)
+
+# Bitwise OR
+bitwise_or_result = m | n
+print("Bitwise OR:", bitwise_or_result)
+
+# Bitwise XOR
+bitwise_xor_result = m ^ n
+print("Bitwise XOR:", bitwise_xor_result)
+
+# Bitwise NOT
+bitwise_not_result = ~m
+print("Bitwise NOT of m:", bitwise_not_result)
+
+# Shift left
+shift_left_result = m << 1
+print("Left shift of m:", shift_left_result)
+
+# Shift right
+shift_right_result = m >> 1
+print("Right shift of m:", shift_right_result)
